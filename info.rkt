4cd4d520243c822097414d4f4ce7eafb37a98467
Vincent St-Amour <stamourv@racket-lang.org>
2015-12-19 17:53:37 -0600

    Move docs to the "Scribble Libraries" category.

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 (define deps '("base" "scribble-lib" "at-exp-lib"))
 (define build-deps '("racket-doc" "scribble-doc"))
 
-(define scribblings '(("lipics.scrbl")))
+(define scribblings '(("lipics.scrbl" () ("Scribble Libraries"))))
 
 (define pkg-desc "Port of the lipics style to Scribble")
 (define pkg-authors '(asumu stamourv))
